b7b921782cf2270d3037f47ca084db02bfab3b60
Eric <horrorhaus666@gmail.com>
2023-02-27 19:48:32 -0300

    pagina de login esperando a parte do back

--- dindin/src/pages/SignIn/index.js
+++ dindin/src/pages/SignIn/index.js
@@ -1,6 +1,25 @@
 import './style.css';
+import { useState } from 'react';
 
 export default function SignIn() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState(false);
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        console.log('Clicou no enviar')
+        if (!email || !password) {
+            setError(true)
+        }
+
+        if (email && password) {
+            setError(false)
+            setEmail('')
+            setPassword('')
+        }
+    }
+
     return (
         <div className='container-sign-in'>
             <div className='sign-in-left'>
@@ -10,10 +29,26 @@ export default function SignIn() {
             </div>
 
             <div className='sign-in-right'>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <h3>Login</h3>
-                    <label>E-mail <input type='email' /></label>
-                    <label>Password <input type='password' /></label>
+                    <label>E-mail
+                        <input
+                            type='email'
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </label>
+
+                    <label>Password
+                        <input
+                            type='password'
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </label>
+
+                    {error && <span className='span-error'>Forneça informações válidas.</span>}
+
                     <button type='submit' className='btn-purple'>Entrar</button>
                 </form>
             </div>
